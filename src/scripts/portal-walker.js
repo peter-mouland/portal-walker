c82797a3c9c0687eb69b6a51d44fc63c1ff07183
Peter Mouland <peter.mouland@gmail.com>
2015-08-20 22:15:56 +0100

    improve positioning

--- src/scripts/portal-walker.js
+++ src/scripts/portal-walker.js
@@ -4,11 +4,14 @@ var stage    = document.querySelector('section.stage');
 var containerStyle = window.getComputedStyle(container) || {};
 var stageStyle = window.getComputedStyle(stage) || {};
 var perspective = parseInt(containerStyle.perspective,10);
+var transform = containerStyle.transform;
+var zoom = transform.indexOf('scale')>-1 ? /\(([^)]+)\)/.exec(transform)[1] : 1;
 var perspectiveOrigin = containerStyle.perspectiveOrigin;
-perspectiveOrigin = perspectiveOrigin ? parseInt(containerStyle.perspectiveOrigin.split(' ')[1],10) : 0;
+perspectiveOrigin = perspectiveOrigin ? parseInt(perspectiveOrigin.split(' ')[1],10) : 0;
 var rotation = -45;//parseInt(stageStyle.transform.split(' ')[1],10); matrix ?
 var verticalJump = 100;
 var horizonJump = 100;
+var zoomJump = 0.1;
 var rotateJump = 1;
 
 function moveCamera(e){
@@ -32,18 +35,12 @@ var move = {
         container.style.perspectiveOrigin = '50% ' + perspectiveOrigin + 'px';
     },
     in: function(){
-        perspective -= horizonJump;
-        container.style.perspective = perspective + 'px';
-
-        perspectiveOrigin += horizonJump/3;
-        container.style.perspectiveOrigin = '50% ' + perspectiveOrigin + 'px';
+        zoom += zoomJump;
+        container.style.transform = 'scale(' + zoom + ')';
     },
     out: function(){
-        perspective += horizonJump;
-        container.style.perspective = perspective + 'px';
-
-        perspectiveOrigin -= verticalJump/3;
-        container.style.perspectiveOrigin = '50% ' + perspectiveOrigin + 'px';
+        zoom -= zoomJump;
+        container.style.transform = 'scale(' + zoom + ')';
     },
     left: function(){
         rotation += rotateJump;
